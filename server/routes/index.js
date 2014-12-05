a1578fdeef3239e7447e3c6a5c7cdd844828643a
danieljensen1987 <cph-dj74@cphbusiness.dk>
2014-12-05 14:09:12 +0100

    login now works with encryptings

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,7 @@ var jwt = require('jsonwebtoken');
 var http = require('http');
 var request = require('request');
 var jpa = require('../model/jpaInterface');
-//var bcrypt = require('bcryptjs');
+var bcrypt = require('bcryptjs');
 
 router.get('/', function(req, res) {
     res.redirect("app/index.html")
@@ -13,14 +13,68 @@ router.get('/', function(req, res) {
 
 router.post('/authenticate', function (req, res) {
     // TODO implement this an make jpa return user instead of role
-    //var loadHash = "";
-    //bcrypt.genSalt(10, function(err, salt) {
-    //    bcrypt.hash(req.body.password, salt, function(err, hash) {
-    //        loadHash = hash;
-    //        console.log(loadHash);
-    //        test();
-    //    });
-    //});
+    var saltedPass = "";
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(req.body.password, salt, function(err, hash) {
+            if(err)console.log("error: " +err);
+            saltedPass = hash;
+
+            var json = {
+                "username": req.body.username,
+                "password": req.body.password
+            };
+
+            jpa.login('/login', json, function (error, data) {
+                if (error){
+                    res.status(401).send('Invalid Username or Password');
+                    return;
+                }
+                console.log('DATA:' + data.id + data.password + data.rolle);
+                bcrypt.compare(req.body.password, data.password, function(err, result) {
+                    console.log("salted pass = " + saltedPass);
+                    console.log("passw from db = " + data.password);
+                    if (result){
+
+
+                        var profile = {
+                            username: req.body.username,
+                            role: data.rolle
+                        };
+
+                        switch (data.rolle){
+                            case 'student':
+                                var token = jwt.sign(profile, require("../security/secrets").secretTokenStudent, { expiresInMinutes: 60*5 });
+                                res.json({ token: token });
+                                break;
+                            case 'teacher':
+                                // teacher role is not yet implemented in secrets and tokens - therefor the role is set to admin
+                                var token = jwt.sign({username: req.body.username, role:'admin'}, require("../security/secrets").secretTokenAdmin, { expiresInMinutes: 60*5 });
+                                res.json({ token: token });
+                                break;
+                            case 'admin':
+                                var token = jwt.sign(profile, require("../security/secrets").secretTokenAdmin, { expiresInMinutes: 60*5 });
+                                res.json({ token: token });
+                                break;
+                            default :
+                                res.status(401).send('Invalid Username or Password');
+                                break;
+                        }
+                    } else{
+                        res.status(401).send('Invalid Username or Password');
+                    }
+
+
+                });
+            });
+
+
+
+            //console.log(saltedPass);
+            //test();
+        });
+    });
+
+
     //var test = function () {
     //    bcrypt.compare(req.body.password, loadHash, function(err, res) {
     //        console.log(loadHash);
@@ -28,40 +82,7 @@ router.post('/authenticate', function (req, res) {
     //    });
     //};
 
-    var json = {
-        "username": req.body.username,
-        "password": req.body.password
-    };
 
-    jpa.login('/login', json, function (error, data) {
-        if (error){
-            res.status(401).send('Invalid Username or Password');
-            return;
-        }
-        var profile = {
-            username: req.body.username,
-            role: data
-        };
-
-        switch (data){
-            case 'student':
-                var token = jwt.sign(profile, require("../security/secrets").secretTokenStudent, { expiresInMinutes: 60*5 });
-                res.json({ token: token });
-                break;
-            case 'teacher':
-                // teacher role is not yet implemented in secrets and tokens - therefor the role is set to admin
-                var token = jwt.sign({username: req.body.username, role:'admin'}, require("../security/secrets").secretTokenAdmin, { expiresInMinutes: 60*5 });
-                res.json({ token: token });
-                break;
-            case 'admin':
-                var token = jwt.sign(profile, require("../security/secrets").secretTokenAdmin, { expiresInMinutes: 60*5 });
-                res.json({ token: token });
-                break;
-            default :
-                res.status(401).send('Invalid Username or Password');
-                break;
-        }
-    });
     //var options = {
     //    uri: 'http://localhost:9191/login',
     //    method: 'POST',
